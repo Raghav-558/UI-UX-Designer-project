4dbf9516cb27ba73432c72986b73925d22c2eedc
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-15 20:36:16 +0530

    add btn

--- src/components/comman/Alittlebit.jsx
+++ src/components/comman/Alittlebit.jsx
@@ -11,8 +11,11 @@ const Alittlebit = () => {
                             <h2 className='eczar text-fs-2xl font-semibold text-white pb-8 max-lg:text-center'>A Little Bit About Me</h2>
                             <p className='font-normal text-xl leading-7 text-white work-sans max-lg:text-center'>Neque lectus elit massa aenean turpis quam aliquet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ipsum, scelerisque proin consequat tristique tortor, amet nullam. Eu purus blandit luctus faucibus justo venenatis a. </p>
                             <div className='max-lg:flex max-lg:justify-center'>
-                                <button className='text-white border-4 border-white hover:text-black hover:bg-white transition-all duration-300 btn-2-pd rounded-md work-sans font-semibold text-fs-sm mt-10 '>Contact Me</button>
+                                <button type='submit' class="work-sans mt-10 font-semibold text-xl rounded-md before:ease relative py-4 px-14 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-300 hover:text-black hover:shadow-blue-500 hover:before:h-64 hover:before:-translate-y-32">
+                                    <span class="relative z-10">Contact Me</span>
+                                </button>
                             </div>
+
                         </div>
                     </div>
                     <div className='w-full lg:w-1/2 px-3 max-lg:mt-5'>
--- src/components/comman/Footer.jsx
+++ src/components/comman/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='bg-black flex items-center justify-center'>
-            <div className='pb-20 pt-72'>
+            <div className='pb-10 pt-10'>
                 <p className='work-sans text-white text-fs-sm font-medium'>© Copyright ChiranjitH.com</p>
             </div>
         </div>
