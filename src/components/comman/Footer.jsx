5c8c0017d5321b72d1440532dcc629dc8f1f4aa1
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-16 10:44:41 +0530

    edit font size

--- src/components/comman/Footer.jsx
+++ src/components/comman/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='flex items-center justify-center'>
-            <div className='pb-10 pt-10'>
+            <div className='pb-10 pt-10 max-sm:pt-5 max-sm:pb-5'>
                 <p className='worksans text-black text-xl font-medium'>© Copyright ChiranjitH.com</p>
             </div>
         </div>
