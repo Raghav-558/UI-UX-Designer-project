537c2c28b2650a62024f6b3fb60bde01232bd502
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-17 10:04:47 +0530

    correct console error

--- src/components/comman/Alittlebit.jsx
+++ src/components/comman/Alittlebit.jsx
@@ -10,8 +10,8 @@ const Alittlebit = () => {
                         <div className='max-lg:pt-10'>
                             <h2 className='eczar text-fs-2xl font-semibold text-white pb-8 max-sm:pb-4 max-lg:pb-6 max-sm:text-3xl'>A Little Bit About Me</h2>
                             <p className='font-normal text-xl leading-7 text-white worksans max-sm:text-base'>Neque lectus elit massa aenean turpis quam aliquet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ipsum, scelerisque proin consequat tristique tortor, amet nullam. Eu purus blandit luctus faucibus justo venenatis a. </p>
-                            <button class="worksans mt-12 max-sm:mt-5 max-lg:mt-8 font-semibold text-xl max-sm:text-base rounded-md before:ease relative py-4 px-14 max-sm:px-6 max-sm:py-3 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
-                                <span class="relative z-10">Contact Me</span>
+                            <button className="worksans mt-12 max-sm:mt-5 max-lg:mt-8 font-semibold text-xl max-sm:text-base rounded-md before:ease relative py-4 px-14 max-sm:px-6 max-sm:py-3 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
+                                <span className="relative z-10">Contact Me</span>
                             </button>
                         </div>
                     </div>
--- src/components/comman/Cards.jsx
+++ src/components/comman/Cards.jsx
@@ -47,8 +47,8 @@ const Cards = () => {
       <div className='max-w-[1140px] mx-auto px-3 pt-16 max-md:pt-10 pb-16 max-md:pb-10'>
         <div className='text-left flex items-center gap-16 max-sm:flex-col max-sm:gap-3'>
           <h2 className='eczar font-semibold text-fs-2xl max-md:text-3xl text-white'>Featured Work</h2>
-          <button class="worksans font-semibold text-xl max-md:text-base max-sm:px-4 max-sm:py-3  rounded-md max-sm:rounded-sm before:ease relative py-4 px-6 overflow-hidden border-4 border-white text-white transition-all duration-500 before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-26 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
-            <span class="relative z-10">View more on Behance</span>
+          <button className="worksans font-semibold text-xl max-md:text-base max-sm:px-4 max-sm:py-3  rounded-md max-sm:rounded-sm before:ease relative py-4 px-6 overflow-hidden border-4 border-white text-white transition-all duration-500 before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-26 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
+            <span className="relative z-10">View more on Behance</span>
           </button>
         </div>
         <div className='flex flex-wrap flex-row -mx-3 pb-30 pt-14 max-sm:pt-6'>
