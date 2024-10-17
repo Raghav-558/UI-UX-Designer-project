e302e95602e19bf8174fe6b1514570a6c41a3761
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-17 16:20:50 +0530

    remove cursior pointer

--- src/components/comman/Cards.jsx
+++ src/components/comman/Cards.jsx
@@ -55,34 +55,36 @@ const Cards = () => {
   };
 
   return (
-    <div className='bg-black'>
-      <div className='max-w-[1140px] mx-auto px-4 pt-16 max-md:pt-10 pb-16 max-md:pb-10 max-lg:px-4'>
+    <div id='work' className='bg-black'>
+      <div className='max-w-[1140px] mx-auto px-5 pt-16 max-md:pt-10 pb-16 max-md:pb-10'>
         <div className='text-left flex items-center gap-16 max-sm:flex-col max-sm:gap-3'>
           <h2 className='eczar font-semibold text-fs-2xl max-md:text-3xl text-white'>Featured Work</h2>
           <button className="worksans font-semibold text-xl max-md:text-base max-sm:px-4 max-sm:py-3 rounded-md max-sm:rounded-sm before:ease relative py-4 px-6 overflow-hidden border-4 border-white text-white transition-all duration-500 before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-26 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
             <span className="relative z-10">View more on Behance</span>
           </button>
         </div>
-        <div className='flex flex-wrap flex-row -mx-3 pt-14 max-sm:pt-6'>
+        <div className='flex flex-wrap flex-row -mx-3 pt-14 max-sm:pt-6 justify-center'>
           {CARDS.slice(0, show).map((obj, index) => (
             <div key={index} className='w-full md:w-1/2 lg:w-1/3 px-3'>
               <a href="#">
-                <div className='bg-white rounded-xl p-2 transition-all duration-300 mb-6 hover:scale-[1.01]'>
-                  <img src={obj.img} alt={obj.title} className='rounded-xl w-full' />
-                  <div className='p-3'>
-                    <h3 className='worksans font-semibold text-2xl max-sm:text-xl'>{obj.title}</h3>
+                <div className='bg-white rounded-md p-2.5 transition-all duration-300 mb-6 hover:scale-[1.01] '>
+                  <div className=''>
+                    <img src={obj.img} alt={obj.title} className='rounded-md w-full' />
+                    <div className='p-3'>
+                      <h3 className='worksans font-semibold text-2xl max-sm:text-xl'>{obj.title}</h3>
+                    </div>
                   </div>
                 </div>
               </a>
             </div>
           ))}
         </div>
-        <div className='flex justify-center items-center'>
-          <button onClick={addCards} className="worksans mt-12 max-sm:mt-5 max-lg:mt-8 font-semibold text-xl max-sm:text-base rounded-md before:ease relative py-3 px-7 max-sm:px-4 max-sm:py-3 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
+        <div className='flex justify-center items-center gap-3'>
+          <button onClick={addCards} className="worksans font-semibold text-xl max-sm:text-base rounded-md before:ease relative py-3 px-7 max-sm:px-4 max-sm:py-3 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
             <span className="relative z-10">Show more...</span>
           </button>
           {show > 3 && (
-            <button onClick={lessCards} className="worksans mt-12 ml-4 max-sm:mt-5 max-lg:mt-8 font-semibold text-xl max-sm:text-base rounded-md before:ease relative py-3 px-7 max-sm:px-4 max-sm:py-3 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
+            <button onClick={lessCards} className="worksans font-semibold text-xl max-sm:text-base rounded-md before:ease relative py-3 px-7 max-sm:px-4 max-sm:py-3 overflow-hidden border-4 border-white text-white transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-white before:duration-500 hover:text-black hover:before:h-64 hover:before:-translate-y-32">
               <span className="relative z-10">Show less...</span>
             </button>
           )}
