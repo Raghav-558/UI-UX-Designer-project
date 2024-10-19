c50982cceaf0c3d5b0d4812eb062a8d2b2beaad9
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-19 09:36:57 +0530

    remove footer aos

--- src/components/comman/Footer.jsx
+++ src/components/comman/Footer.jsx
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 
 const Footer = () => {
-    useEffect(() => {
-        AOS.init({
-            duration: 1200,
-        });
-    }, []);
+
 
     return (
-        <div data-aos="fade-right" className='flex items-center justify-center'>
+        <div className='flex items-center justify-center'>
             <div className='pb-10 pt-10 max-md:pt-5 max-md:pb-5  max-sm:pt-3 max-sm:pb-3'>
                 <p className='font-worksans text-black text-xl font-medium max-sm:text-base'>© Copyright ChiranjitH.com</p>
             </div>
